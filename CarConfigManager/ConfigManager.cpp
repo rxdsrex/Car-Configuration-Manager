4aa5d35d0ad6d6f3dfe0f36e73bfe3d6cefec6ec
Sushant Adhikari <sushantadh@gmail.com>
2017-11-30 19:58:34 +0530

    removed content from configManager

--- CarConfigManager/ConfigManager.cpp
+++ CarConfigManager/ConfigManager.cpp
@@ -2,25 +2,4 @@
 
 class ConfigManager {
 
-private:
-    string engine
-public:
-
-};
-
-class InternalConfig:public ConfigManager {
-
-};
-
-class ExternalConfig:public ConfigManager {
-
-};
-
-int main(int argc, char* argv[]){
-
-    ConfigManager myConfigManager;
-    myConfigManager.handler = new JSONHandler();
-    myConfigManager.jsonConfig = myConfigManager.handler->readFromFile("../internal.json");
-    myConfigManager.handler->writeToFile(myConfigManager.jsonConfig,"testfile.json");
-    return 0;
 }
